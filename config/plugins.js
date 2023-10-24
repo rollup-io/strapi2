d38ac7e2e8fda0b66a14c5dda0f3923861a34545
Bohdan Tokarskyi <bt@rollup.ai>
2023-10-24 18:38:03 +0300

    remove import plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,21 @@
-module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
+module.exports = ({env}) => ({
+  'users-permissions': {
+    config: {
       jwtSecret: env('JWT_SECRET'),
-      },
     },
-  'import-export-entries': {
-    enabled: true,
   },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-  });
+  },
+});
